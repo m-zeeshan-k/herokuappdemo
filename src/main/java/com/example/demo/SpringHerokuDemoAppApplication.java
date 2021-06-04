c3ac80fee3389ca64e409f48f57ad0224deb6f0d
Zeeshan.Khan <zishaan746@gmail.com>
2021-06-04 23:56:19 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringHerokuDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHerokuDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHerokuDemoAppApplication.class, args);
+	}
+
+}
