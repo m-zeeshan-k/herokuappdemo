e7e14140d2821a0a01578c25a2a036cb7eb59267
Zeeshan.Khan <zishaan746@gmail.com>
2021-06-06 00:36:08 +0530

    str

--- src/main/java/com/example/demo/controller/MyController.java
+++ src/main/java/com/example/demo/controller/MyController.java
@@ -3,23 +3,24 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dao.UserRepository;
 import com.example.demo.entity.User;
 
-@Controller
+@RestController
 public class MyController {
 
 	@Autowired
 	UserRepository userRepository;
 	
-	@RequestMapping("/")
+	@GetMapping("/")
 	public String loadHome() {
 	return "home";	
 	}
